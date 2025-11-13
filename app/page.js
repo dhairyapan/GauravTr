748ec299234d7e702692ac175ae190cc8c465ac9
Dhairya Pandey <dhairyapandey585@gmail.com>
2025-11-13 18:57:10 +0530

    Updated home section or new feature added

--- app/page.js
+++ app/page.js
@@ -4,6 +4,12 @@ import Aboutus from "@/components/HomeAbout"
 import PopularProducts from "@/components/popularProduct";
 import HomeContact from "@/components/HomeContact"
 
+export const metadata = {
+  title: "Gaurav Trading Co. | glassware manufacturers",
+  description:
+    "Buy high-quality borosil glassware including beakers, test tubes, flasks, measuring cylinders, and more at best prices. Gaurav Trading Co. delivers durable, heat-resistant lab glassware with safe packaging and pan-India delivery.",
+};
+
 export default function Home() {
   return (
     <>
